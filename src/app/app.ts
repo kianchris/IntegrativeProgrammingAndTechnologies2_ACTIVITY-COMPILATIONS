f04b5e2402c38e9a5fc0226cc2df822fb909a434
kianchris <kianchris.gio@evsu.edu.ph>
2026-02-21 11:11:37 +0800

    My second Activity

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,35 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [FormsModule, CommonModule],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('My_Login_Form');
+
+  username = '';
+  password = '';
+  message = '';
+
+  validatePassword(pass: string): boolean {
+    const upper = /[A-Z]/;
+    const lower = /[a-z]/;
+    const number = /[0-9]/;
+
+    return upper.test(pass) && lower.test(pass) && number.test(pass);
+  }
+
+  login() {
+    if (!this.validatePassword(this.password)) {
+      this.message = "Password must contain uppercase, lowercase, and number!";
+      return;
+    }
+
+    this.message = "Welcome, " + this.username + "!";
+  }
+
 }
